cc71fe624f41f7db0d52de332a875a061a1f5758
ZayrexDev <zay.rex@qq.com>
2026-04-16 15:15:42 +0800

    Debugging

--- src/main/java/xyz/zcraft/service/BeatmapCacheService.java
+++ src/main/java/xyz/zcraft/service/BeatmapCacheService.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import xyz.zcraft.network.OsuAPI;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -31,6 +32,12 @@ public class BeatmapCacheService {
         }
 
         try {
+            byte[] bytes = Files.readAllBytes(BEATMAP_CACHE.resolve(id));
+            System.out.println("beatmap bytes len=" + bytes.length);
+            System.out.println("first 32 bytes=" + new String(bytes, 0, 32, StandardCharsets.UTF_8));
+            System.out.println("last 32 bytes=" + new String(bytes, Math.max(0, bytes.length - 32), Math.min(32, bytes.length), StandardCharsets.UTF_8));
+
+
             return Files.readAllBytes(BEATMAP_CACHE.resolve(id));
         } catch (Exception e) {
             LOG.error("Failed to load beatmap from cache!", e);
